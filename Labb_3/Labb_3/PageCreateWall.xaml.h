f14e068665a4f206ae2034fd3a390349a3e3d4e6
RasmusLundquist <Rasmus.lundquist@hotmail.com>
2016-03-25 15:02:41 +0100

    Vy för att skapa väggar

---
+++ Labb_3/Labb_3/PageCreateWall.xaml.h
@@ -0,0 +1,21 @@
+﻿//
+// PageCreateWall.xaml.h
+// Declaration of the PageCreateWall class
+//
+
+#pragma once
+
+#include "PageCreateWall.g.h"
+
+namespace Labb_3
+{
+	/// <summary>
+	/// An empty page that can be used on its own or navigated to within a Frame.
+	/// </summary>
+	[Windows::Foundation::Metadata::WebHostHidden]
+	public ref class PageCreateWall sealed
+	{
+	public:
+		PageCreateWall();
+	};
+}
